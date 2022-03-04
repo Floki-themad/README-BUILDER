184cd0f694c0f3660198a9cdf3b3d6d295aeb218
Floki <ypr_tylerkeuler@aol.com>
2022-03-04 12:49:06 -0700

    working on readme issues

---
+++ README.md
@@ -0,0 +1,29 @@
+# README builder
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+## Description
+a dynamically changing readme generator built using javascript and node.js
+## Table of Contents
+* [Installation](#installation)  
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation
+To install necessary dependencies, run the following command:
+'''
+npm inquirer
+'''
+## Usage
+a straight foward way to making a quick and professional readme
+## License
+This project is licensed under the MIT license.
+## Contributing
+please follow best coding practices if contributing
+## Tests
+To run tests, run the following command:
+'''
+node index.js
+'''
+## Questions
+If you have any questions about the repo, open an issue or contact me directly at ypr_tylerkeuler@aol.com. Youcan find more of my work at [Floki-themad](https://github.com/Floki-themad/).
--- index.js
+++ index.js
@@ -1,69 +1,127 @@
-var fileGenerator = require("./fileGenerator");
-var fs = require("fs");
-var inquirer = require('inquirer');
 
-// questions to ask the user //
-let questions = [
-    {
-        type: "input",
-        message: "What is the title of your repository?",
-        name: "title"
-    },{
-        type: "input",
-        message: "Please give your GitHub profile link.",
-        name: "GitHub"
-    },{
-        type: "input",
-        message: "What is your email?",
-        name: "email"
-    },{
-        type: "list",
-        name: "license",
-        message: "Please select which license you would like to use.",
-        choices : [
-            "APACHE 2.O",
-            "BSD 3",
-            "GVL-GPL 3.0",
-            "MIT",
-            "None"
-        ],
-    },{
-        type: "input",
-        message: "Please describe the repository.",
-        name: "description"
+// declaring variables that node will be accessing
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-    },{
-        type: "input",
-        message: "Installation Instructions",
-        name: "installation"
-    },{
-        type: "input",
-        message: "Please state if others can contribute.",
-        name: "contribute"
-    },{
-        type: "input",
-        message: "Please state any test(s) require.",
-        name: "test"
-    },{
-        type: "input",
-        message: "Please provide a screenshot.",
-        name: "scriptjs"
-    },{
-        type: "input",
-        message: "Questions?",
-        name: "questions"
-    }
-];
+// calling inquirer. This will go though and prompt the user with questions
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'github',
+        },
+        {
+            type: 'input',
+            message: "What is your email address?",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "What is your project's name?",
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: "Please write a short description of your project",
+            name: 'description',
+        },
+        {
+            type: 'list',
+            message: "What kind of license should your project have?",
+            name: 'license',
+            choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "none"]
+        },
+        {
+            type: 'input',
+            message: "What command should be run to install dependecies?",
+            name: 'install',
+        },
+        {
+            type: 'input',
+            message: "What command should be run to run tests?",
+            name: 'test',
+        },
+        {
+            type: 'input',
+            message: "What does the user need to know about using your repo?",
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: "What does the user need to know about contributing to your repo?",
+            name: 'contribution',
+        },
+    ])
+
+    // after all of the prompts have been given, the information the user inputs will be taken as data
+    .then((data) => {
+
+// this function takes in the information given from the user and returns the contents of the readme.
+        function renderReadme(title, license, description, install, usage, contribution, test, email, github) {
+            
+            // declaring the variables that will be used in the function
+            let licenseBadge = "";
+            let licenseDiscrption = ""
+
+            // this swtich will determine which license badge and license discription will be used based on what the user chose.
+            switch (license) {
+                case "MIT": licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+                    licenseDiscrption = "This project is licensed under the MIT license."
+                    break;
+                case "APACHE 2.0": licenseBadge = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+                    licenseDiscrption = "This project is licensed under the APACHE 2.0 license."
+                    break;
+                case "GPL 3.0": licenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+                    licenseDiscrption = "This project is licensed under the GPL 3.0 license."
+                    break;
+                case "BSD 3": licenseBadge = "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+                    licenseDiscrption = "This project is licensed under the BSD 3 license."
+                    break;
+                case "none": licenseBadge = ""
+                    licenseDiscrption = "This project is not under any licenses."
+                    break;
+                default: licenseBadge = ""
+                    licenseDiscrption = "This project is not under any licenses."
+                    break;
+            }
 
 
-// Function to write to my ReadMe.md file. //
-inquirer.prompt(questions).then(function(response) {
-   console.log(response);
-   
-    var content = fileGenerator(response);
-    console.log(content);
-     fs.writeFile("./ReadMe.md", content, function(err){
-         if (err) throw err
-         console.log("success");
-     });
-} );
+// this returns the readme format and takes in the user input.
+            return `# ${title}
+${licenseBadge}
+## Description
+${description}
+## Table of Contents
+* [Installation](#installation)  
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation
+To install necessary dependencies, run the following command:
+'''
+${install}
+'''
+## Usage
+${usage}
+## License
+${licenseDiscrption}
+## Contributing
+${contribution}
+## Tests
+To run tests, run the following command:
+'''
+${test}
+'''
+## Questions
+If you have any questions about the repo, open an issue or contact me directly at ${email}. Youcan find more of my work at [${github}](https://github.com/${github}/).
+`}
+
+// this appends the file README.md and calls the function to render it.
+        fs.appendFile("README.md", (renderReadme(data.name, data.license, data.description, data.install, data.usage, data.contribution, data.test, data.email, data.github)), (err) =>
+            err ? console.log(err) : console.log("SUCCESS")
+        );
+
+    });
