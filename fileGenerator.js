0756b87a009916639583543670ed710b4cdb219a
Floki <ypr_tylerkeuler@aol.com>
2022-03-04 10:29:08 -0700

    adding first few questions into the index.js

--- fileGenerator.js
+++ fileGenerator.js
@@ -1,3 +1,37 @@
 var fileGenerator = require("./fileGenerator");
 var fs = require("fs");
 var inquirer = require('inquirer');
+
+// User questions
+let questions = [
+    {
+        type: "input",
+        message: "What is the title of your repository?",
+        name: "title"
+    },{
+        type: "input",
+        message: "Please give your logo information.",
+        name: "logo"
+    },{
+        type: "input",
+        message: "What is your GitHub user name?",
+        name: "userName"
+    },{
+        type: "input",
+        message: "Please give your GitHub profile link.",
+        name: "GitHub"
+    },{
+        type: "input",
+        message: "What is your email?",
+        name: "email"
+    },{
+        type: "list",
+        name: "license",
+        message: "Please select which license you would like to use.",
+        choices : [
+            "APACHE 2.O",
+            "BSD 3",
+            "GVL-GPL 3.0",
+            "MIT",
+            "None"
+        ],
